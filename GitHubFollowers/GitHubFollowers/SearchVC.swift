796895b3104704466252ab7c423ea44364d2c8af
Dan M <djmthedev@gmail.com>
2020-02-13 15:28:52 -0800

    added configureLogoImageView

--- GitHubFollowers/GitHubFollowers/SearchVC.swift
+++ GitHubFollowers/GitHubFollowers/SearchVC.swift
@@ -17,6 +17,7 @@ class SearchVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureLogoImageView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,5 +25,16 @@ class SearchVC: UIViewController {
         navigationController?.isNavigationBarHidden = true
     }
     
-    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
 }
