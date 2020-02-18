04465d0cbf6c03d2a11795d3a9b137111c7fa029
Dan M <djmthedev@gmail.com>
2020-02-18 06:03:47 -0800

    handled data, response, and error in dataTask

--- GitHubFollowers/GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/GitHubFollowers/Managers/NetworkManager.swift
@@ -23,8 +23,33 @@ class NetworkManager {
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             
+            if let _ = error {
+                completed(nil, "Unable to complete your request. Please check your internet connection")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                response.statusCode == 200 else {
+                    completed(nil, "Invalid response from the server. Please try again.")
+                    return
+            }
+            
+            guard let data = data else {
+                completed(nil, "The data received from the server was invalid. Please try again")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                
+                completed(followers, nil)
+                
+            } catch {
+                completed(nil, "The data received from the server was invalid. Please try again")
+            }
         }
-        
         task.resume()
     }
 }
