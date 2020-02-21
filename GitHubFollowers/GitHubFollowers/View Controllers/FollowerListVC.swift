ab391713dbad73fffe18ad358946400139589fbd
Dan Morse <djmthedev@gmail.com>
2020-02-21 07:34:34 -0800

    added loading screen

--- GitHubFollowers/GitHubFollowers/View Controllers/FollowerListVC.swift
+++ GitHubFollowers/GitHubFollowers/View Controllers/FollowerListVC.swift
@@ -50,8 +50,10 @@ class FollowerListVC: UIViewController {
     }
     
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             
             switch result {
             case .success(let followers):
@@ -99,7 +101,6 @@ extension FollowerListVC: UICollectionViewDelegate {
             page += 1
             getFollowers(username: username, page: page)
         }
-        
     }
     
     
