90728b7a97730c9f92857d36e50fbc15c367ab60
Dan Morse <djmthedev@gmail.com>
2020-02-24 07:36:23 -0800

    added Constants file with SFSymbols

---
+++ GitHubFollowers/GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by morse on 2/24/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
