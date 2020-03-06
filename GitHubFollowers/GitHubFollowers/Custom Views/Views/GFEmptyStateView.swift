1e3b73885414f0580abee2765bdac52ed6ffabf0
Dan Morse <djmthedev@gmail.com>
2020-03-06 10:31:13 -0800

    refactored empty state view refactored GFDataLoadingVC changed UIHelper to an enum instead of a struct

--- GitHubFollowers/GitHubFollowers/Custom Views/Views/GFEmptyStateView.swift
+++ GitHubFollowers/GitHubFollowers/Custom Views/Views/GFEmptyStateView.swift
@@ -12,7 +12,7 @@ class GFEmptyStateView: UIView {
     
     let messageLabel = GFTitleLabel(textAlignment: .center, fontSize: 28)
     let logoImageView = UIImageView()
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -28,25 +28,41 @@ class GFEmptyStateView: UIView {
     }
     
     private func configure() {
+        configureMessageLabel()
+        configureLogoImageView()
+    }
+    
+    private func configureMessageLabel() {
         addSubview(messageLabel)
-        addSubview(logoImageView)
-        
         messageLabel.numberOfLines = 3
         messageLabel.textColor = .secondaryLabel
         
+        let labelCenterYConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? -90 : -150
+        let messageLabelCenterYContstraint = messageLabel.centerYAnchor.constraint(
+            equalTo: self.centerYAnchor,
+            constant: labelCenterYConstant)
+        
+        NSLayoutConstraint.activate([
+            messageLabelCenterYContstraint,
+            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    private func configureLogoImageView() {
+        addSubview(logoImageView)
         logoImageView.image = Images.emptyStateLogo
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         
+        let logoBottomContstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 80 : 40
+        let logoImageBottomContstraint = logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: logoBottomContstant)
+        
         NSLayoutConstraint.activate([
-            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
-            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
-            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
-            messageLabel.heightAnchor.constraint(equalToConstant: 200),
-            
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
-            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
+            logoImageBottomContstraint
         ])
     }
 }
