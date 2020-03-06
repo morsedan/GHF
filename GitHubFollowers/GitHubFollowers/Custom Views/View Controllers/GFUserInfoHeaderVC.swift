270c769867fc76472b9e7bf8eafb5835151c264c
Dan Morse <djmthedev@gmail.com>
2020-03-06 09:31:40 -0800

    added image fetch to user info header vc

--- GitHubFollowers/GitHubFollowers/Custom Views/View Controllers/GFUserInfoHeaderVC.swift
+++ GitHubFollowers/GitHubFollowers/Custom Views/View Controllers/GFUserInfoHeaderVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class GFUserInfoHeaderVC: UIViewController {
-
+    
     let avatarImageView = GFAvatarImageView(frame: .zero)
     let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 34)
     let nameLabel = GFSecondaryTitleLabel(fontSize: 18)
@@ -30,14 +30,14 @@ class GFUserInfoHeaderVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         addSubviews()
         layoutUI()
         configureUIElements()
     }
     
     func configureUIElements() {
-        avatarImageView.downloadImage(from: user.avatarUrl)
+        downloadAvatarImage()
         usernameLabel.text = user.login
         nameLabel.text = user.name ?? ""
         locationLabel.text = user.location ?? "No Location"
@@ -46,6 +46,15 @@ class GFUserInfoHeaderVC: UIViewController {
         
         locationImageView.image = UIImage(systemName: SFSymbols.location)
         locationImageView.tintColor = .secondaryLabel
+    }
+    
+    func downloadAvatarImage() {
+        NetworkManager.shared.downloadImage(from: user.avatarUrl) { [weak self] image in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.avatarImageView.image = image
+            }
+        }
     }
     
     func addSubviews() {
@@ -94,5 +103,5 @@ class GFUserInfoHeaderVC: UIViewController {
             bioLabel.heightAnchor.constraint(equalToConstant: 60)
         ])
     }
-
+    
 }
