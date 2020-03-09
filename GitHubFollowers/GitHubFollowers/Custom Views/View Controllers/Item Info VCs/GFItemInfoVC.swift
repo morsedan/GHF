7364a14f529daf2e8d363e79f157c3c1603cf25d
Dan Morse <djmthedev@gmail.com>
2020-03-09 07:03:24 -0700

    moved and renamed ItmeInfoVCDelegate

--- GitHubFollowers/GitHubFollowers/Custom Views/View Controllers/Item Info VCs/GFItemInfoVC.swift
+++ GitHubFollowers/GitHubFollowers/Custom Views/View Controllers/Item Info VCs/GFItemInfoVC.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+protocol ItemInfoVCDelegate: class {
+    func didTapGitHubProfile(for user: User)
+    func didTapGetFollowers(for user: User)
+}
+
 class GFItemInfoVC: UIViewController {
 
     let stackView = UIStackView()
@@ -16,7 +21,7 @@ class GFItemInfoVC: UIViewController {
     let actionButton = GFButton()
     
     var user: User!
-    weak var delegate: UserInfoVCDelegate!
+    weak var delegate: ItemInfoVCDelegate!
     
     init(user: User) {
         super.init(nibName: nil, bundle: nil)
--- GitHubFollowers/GitHubFollowers/View Controllers/UserInfoVC.swift
+++ GitHubFollowers/GitHubFollowers/View Controllers/UserInfoVC.swift
@@ -8,11 +8,6 @@
 
 import UIKit
 
-protocol UserInfoVCDelegate: class {
-    func didTapGitHubProfile(for user: User)
-    func didTapGetFollowers(for user: User)
-}
-
 class UserInfoVC: GFDataLoadingVC {
     
     let headerView = UIView()
@@ -109,7 +104,7 @@ class UserInfoVC: GFDataLoadingVC {
 
 }
 
-extension UserInfoVC: UserInfoVCDelegate {
+extension UserInfoVC: ItemInfoVCDelegate {
     func didTapGitHubProfile(for user: User) {
         guard let url = URL(string: user.htmlUrl) else {
             presentGFAlertOnMainThread(title: "Invalid URL", message: "The url attached to this user is invalid", buttonTitle: "Ok")
